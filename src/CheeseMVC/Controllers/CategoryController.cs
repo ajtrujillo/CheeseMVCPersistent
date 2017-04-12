b7ad49453f0901e8d296b1f0ad1a49ebdfb38994
A.J. Trujillo <ajtrjll@gmail.com>
2017-04-12 18:46:21 -0500

    Initial commit

---
+++ src/CheeseMVC/Controllers/CategoryController.cs
@@ -0,0 +1,32 @@
+﻿using CheeseMVC.Data;
+using CheeseMVC.Models;
+using CheeseMVC.Data;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+
+namespace CheeseMVC.Controllers
+{
+    public class CategoryController: Controller
+    {
+        private readonly CheeseDbContext context;
+
+        public CategoryController(CheeseDbContext dbContext)
+        {
+            context = dbContext;
+        }
+    }
+
+    //GET: /<controller>/
+
+    public IActionResult Index()
+    {
+        List<CategoryController> categories = context.Categories.ToList();
+
+        return View(categories);
+    }
+
+}
